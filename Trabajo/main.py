6f111bbdc7564f587010425ae3aae361beb623b4
Guisplay <148806563+Guisplay@users.noreply.github.com>
2024-02-25 11:43:49 +0100

    Update main.py

--- Trabajo/main.py
+++ Trabajo/main.py
@@ -1,7 +1,6 @@
 import sys
 import pygame as pg
 from Pantalla import Pantalla
-from Juego import Juego
 
 class Game:
     def __init__(self, WIDTH, HEIGTH):
@@ -10,13 +9,11 @@ class Game:
         self.pantalla = pg.display.set_mode((WIDTH, HEIGTH))
         self.clock = pg.time.Clock()
         
-        self.juego = Juego()
         self.pantalla_actual = Pantalla(self.pantalla)
 
     def run(self, FPS):
         self.ejecutando = True
         while self.ejecutando:    
-            self.juego.jugar()
             print('A')
             self.pantalla_actual.update()
             self.clock.tick(FPS)
