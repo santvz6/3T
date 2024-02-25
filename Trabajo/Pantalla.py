d3e1eddbe852be7b0b2cd6269067b8134c0d5b05
Guisplay <148806563+Guisplay@users.noreply.github.com>
2024-02-25 11:44:30 +0100

    Update Pantalla.py

--- Trabajo/Pantalla.py
+++ Trabajo/Pantalla.py
@@ -1,53 +1,41 @@
 import pygame as pg
 import sys
-from Reglas import reglas
+from T1_settings import Tablero1
+import cte
 
 # Pantalla se encargará de establecer cada tipo de escenario y de
 # seleccionar mediante update() que escenario cargar
+
 class Pantalla:
-    BLANCO = (255,255,255)
-    ROJO = (255,0,0)
     def __init__(self, pantalla):
-        self.cambio_pantalla = '1t'
-        self.pantalla = pantalla
-        self.reglas = reglas # instancia de settings1
 
-    def mostrar_texto(self, texto, fuente, tamaño, color, posicion):
-        # Crear un objeto de texto
-        font = pg.font.Font(fuente, tamaño)
-        text_surface = font.render(texto, True, color)
+        # Atributos de instancia
+        self.pantalla = pantalla 
+        self.cambio_pantalla = '1t' # pantalla actual
 
-        # Obtener el rectángulo del texto y configurar la posición
-        text_rect = text_surface.get_rect()
-        text_rect.topleft = posicion
-
-        # Dibujar el texto en la pantalla
-        self.pantalla.blit(text_surface, text_rect)
-
-    def tablero_pg(self):
-        self.fondo_1t = pg.image.load('T1/Imagenes/1t.png')
-        self.fuente_p1 = ('T1/Fuentes/P1.ttf')
-        self.pantalla.blit(self.fondo_1t,(0,0))
-        for y in range(3):
-            for x in range(3):
-                self.mostrar_texto('X',self.fuente_p1,30,Pantalla.ROJO,(560+82*x,260+80*y))
-
-        
-
-
-    def prueba(self):
-        self.pantalla.fill((100,100,100))
+        # Instancias
+        self.t1_set = Tablero1(self.pantalla)
 
     def update(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
-            
+
+            if self.cambio_pantalla == '1t':
+                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:  
+                    if not self.t1_set.victoria_1t():                     
+                        self.t1_set.actualizar_1t()
+                    
+
         if self.cambio_pantalla == '1t':
-            self.tablero_pg() # Dibujo del fondo
+            if not self.t1_set.victoria_1t():
+                self.t1_set.dbujar_1t() # Dibujo del fondo
+            
+            else:
+                self.cambio_pantalla = 'p'
 
         
         elif self.cambio_pantalla == 'p':
-            self.prueba()
+            self.pantalla.fill(cte.BLANCO)
 
         pg.display.update()
