7072a7725a81a408092990d44751153b314a0990
Guisplay <148806563+Guisplay@users.noreply.github.com>
2024-04-24 21:07:58 +0200

    Delete Trabajo/Pruebas directory

--- Trabajo/Pruebas/24_02/Jugador.py
+++
@@ -1,4 +0,0 @@
-class Jugador:
-  def __init__(self, nombre, simbolo):
-    self.nombre = nombre
-    self.simbolo = simbolo
--- Trabajo/Pruebas/25_02/Jugador.py
+++
@@ -1,5 +0,0 @@
-class Jugador:
-  def __init__(self, nombre, simbolo, puntuacion):
-      self.nombre = nombre
-      self.simbolo = simbolo
-      self.puntuacion = puntuacion
