8e492984ebb96900a9c2dc99956db93a6f22402c
Guisplay <148806563+Guisplay@users.noreply.github.com>
2024-04-25 16:25:06 +0200

    Create __init__.py

---
+++ UI_db/__init__.py
@@ -0,0 +1,9 @@
+# YouTube.
+# NeuralNine (2022). https://www.youtube.com/watch?v=GxCXiSkm6no
+
+
+# Usando __init__.py:
+# Convertimos este directorio en un módulo
+
+__all__ = ['DataBase','ui_login','ui_menu']    # from UI_db import * 
+                                                # * → hace referencia a __all__
