2e7a8b56fa68f1dd99bce5f77b6a568eb5c6b79b
Damag0 <153758175+Damag0@users.noreply.github.com>
2024-05-10 10:52:25 +0200

    Add files via upload

--- Settings/T3_settings.py
+++ Settings/T3_settings.py
@@ -20,7 +20,7 @@ class Tablero3:
         # Numpy trata las cadenas de caracteres como matrices de caracteres Unicode.
         # https://stackoverflow.com/questions/55377213/numpy-taking-only-first-character-of-string
 
-        self.tablero = np.array([[[[[[str((v+1)+(u*3)) for v in range(3)] for u in range(3)] for j in range(3)] for i in range(3)] for t in range(3)] for k in range(3)], 
+        self.tablero = np.array([[[[[[str((v+1)+(u*3)) for v in range(3)] for u in range(3)] for j in range(3)] for i in range(3)] for t in range(3)] for k in range(3)],
                                 dtype=np.dtype('U2')) # Establecemos la longitud de datos hasta 2 (usaremos 'J1' y 'J2')
 
         # i → filas                 para acceder a un elemento → [u, v, k, t, i, j],
@@ -44,7 +44,8 @@ class Tablero3:
         self.num_mov = 0                # de momento no lo hemos usado, pero es recomendable implementarlo
         self.movimiento = (-1,-1,-1,-1) # guarda la restricción de movimiento (m_fila, m_columna) que hay que jugar
                                         # (-1,-1,-1,-1) indica que no hay restricción de movimiento
-
+        self.mini_victorias_1T = []
+        self.mini_victorias_2T = []
     ########################### LÓGICA DE TURNOS ###########################
     # Mismo sistema que en 2T
     def turno(self):
@@ -58,7 +59,7 @@ class Tablero3:
 
     # SÓLAMENTE se llama a la función cuando se realice un cambio sobre el tablero
     # de esta forma ahorramos recursos y optimizamos nuestro juego
-    def dibujar_3t(self, m_victorias: list, M_victorias: list):
+    def dibujar_3t(self):
         self.pantalla.blit(cte.fondo_3t,(0,0))
         
         for M_fila in range(3):
@@ -67,15 +68,22 @@ class Tablero3:
                     for m_columna in range(3):
                         for fila in range(3):
                             for columna in range(3):
-                                x = 280 + 242 * M_columna + 82 * m_columna + 80 / 3 * columna  # la 'x' se mueve por columnas
-                                y = 0 + 240 * M_fila + 80 * m_fila + 80 / 3 * fila  # la 'y' se mueve por filas
-                                self.mostrar_texto(self.pantalla_trans, self.tablero[M_fila, M_columna, m_fila, m_columna, fila, columna],  cte.fuente_p1, cte.BLANCO2_T, (x, y))
+                                x = 282 + 242 * M_columna + 82 * m_columna + 80 / 3 * columna  # la 'x' se mueve por columnas
+                                y = -3 + 240 * M_fila + 80 * m_fila + 80 / 3 * fila  # la 'y' se mueve por filas
+                                match self.tablero[M_fila, M_columna, m_fila, m_columna, fila, columna]:
+                                    case self.jugador1.simbolo:
+                                        color = (255, 0, 0, 100)
+                                    case self.jugador2.simbolo:
+                                        color = (0, 0, 255, 100)
+                                    case _:
+                                        color = cte.BLANCO2_T
+                                self.mostrar_texto(self.pantalla_trans, self.tablero[M_fila, M_columna, m_fila, m_columna, fila, columna],  cte.fuente_p1, 26, color, (x, y))
 
 
 
     # Propósito: SÓLO para iluminar por dónde se desliza el cursor
     # Está ejecutandose constantemente (tiene un único propósito para no ocasionar lag)
-    def dibujar_3t_on(self, mini_victorias:list):
+    def dibujar_3t_on(self):
             m_pos = pg.mouse.get_pos()                                                             
             self.pantalla.blit(cte.fondo_3t,(0,0))
 
@@ -86,8 +94,8 @@ class Tablero3:
                             for m_columna in range(3):
                                 for fila in range(3):
                                     for columna in range(3):
-                                        x = 280 + 242*M_columna + 82*m_columna + 80/3*columna   # la 'x' se mueve por columnas
-                                        y = 0 + 240*M_fila + 80*m_fila + 80/3*fila   
+                                        x = 282 + 242*M_columna + 82*m_columna + 80/3*columna   # la 'x' se mueve por columnas
+                                        y = -3 + 240*M_fila + 80*m_fila + 80/3*fila
                                          
                                         if x < m_pos[0] < x+80/3  and y < m_pos[1] < y+80/3:  
                                                       # la 'y' se mueve por filas
@@ -97,11 +105,11 @@ class Tablero3:
 
     ########################### REGISTRO DE JUGADAS REALIZADAS Y PRÓXIMAS JUGADAS ###########################
 
-    def definir_restriccion(self, M_fila:int, M_columna:int, m_fila:int, m_columna:int, mini_victorias_1T:list, mini_victorias_2T:list):
+    def definir_restriccion(self, M_fila:int, M_columna:int, m_fila:int, m_columna:int):
         # Se establece la restriccion si el movimiento anterior no esta en un tablero ganado
 
-        if (M_fila, M_columna) not in mini_victorias_2T:
-            if (M_fila, M_columna, m_fila, m_columna) not in mini_victorias_1T:
+        if (M_fila, M_columna) not in self.mini_victorias_2T:
+            if (M_fila, M_columna, m_fila, m_columna) not in self.mini_victorias_1T:
                 self.movimiento = (M_fila, M_columna, m_fila, m_columna)
                 self.turno()
 
@@ -115,7 +123,10 @@ class Tablero3:
 
     # Actualizacion del tablero al hacer click
 
-    def actualizar_3t_mouse(self, mini_victorias_1T: list, mini_victorias_2T:list):
+    def actualizar_3t_mouse(self):
+        # Usamos teclas
+
+        # Usamos el mouse
         m_pos = pg.mouse.get_pos()  # obtenemos la posición del mouse cuando se hizo click
         for M_fila in range(3):
             for M_columna in range(3):
@@ -140,19 +151,44 @@ class Tablero3:
                                 # Calculamos la posición de cada número
                                 if x < m_pos[0] < x + 80 / 3 and y < m_pos[1] < y + 80 / 3:
                                     # Solo si el tablero a jugar es valido
-                                    if (M_fila, M_columna) not in mini_victorias_2T:
-                                        if (M_fila, M_columna, m_fila, m_columna) not in mini_victorias_1T:
+                                    if (M_fila, M_columna) not in self.mini_victorias_2T:
+                                        if (M_fila, M_columna, m_fila, m_columna) not in self.mini_victorias_1T:
 
                                             # Movimiento libre
                                             if self.movimiento == (-1, -1, -1, -1):
 
                                                 if self.tablero[M_fila, M_columna, m_fila, m_columna, fila, columna] in [str(_ + 1) for _ in range(9)]:
-                                                    self.tablero[M_fila, M_columna, m_fila, m_columna, fila, columna]
+                                                    self.tablero[M_fila, M_columna, m_fila, m_columna, fila, columna] = self.actual.simbolo
+                                                    self.mini_victorias_1T = self.get_mini_victorias_1T()
+                                                    self.mini_victorias_2T = self.get_mini_victorias_2T(self.mini_victorias_1T)
+                                                    self.definir_restriccion(m_fila, m_columna, fila, columna)
+                                                    self.num_mov += 1
+
+                                            # Movimiento con semirestriccion
+                                            elif self.movimiento == (M_fila, M_columna, -1, -1):
+
+                                                if self.tablero[self.movimiento[0], self.movimiento[1], m_fila, m_columna, fila, columna] in [str(_ + 1) for _ in range(9)]:
+                                                    self.tablero[self.movimiento[0], self.movimiento[1], m_fila, m_columna, fila, columna] = self.actual.simbolo
+                                                    self.mini_victorias_1T = self.get_mini_victorias_1T()
+                                                    self.mini_victorias_2T = self.get_mini_victorias_2T(self.mini_victorias_1T)
+                                                    self.definir_restriccion(m_fila, m_columna, fila, columna)
+                                                    self.num_mov += 1
+
+                                            # Movimiento completamente restringido
+                                            else:
+
+                                                 if self.tablero[self.movimiento[0], self.movimiento[1], self.movimiento[2], self.movimiento[3], fila, columna] in [str(_ + 1) for _ in range(9)]:
+                                                    self.tablero[self.movimiento[0], self.movimiento[1], self.movimiento[2], self.movimiento[3], fila, columna] = self.actual.simbolo
+                                                    self.mini_victorias_1T = self.get_mini_victorias_1T()
+                                                    self.mini_victorias_2T = self.get_mini_victorias_2T(self.mini_victorias_1T)
+                                                    self.definir_restriccion(self.movimiento[2], self.movimiento[3], fila, columna)
+                                                    self.num_mov += 1
+
 
 
     ########################### EJECUCIÓN DE T3 ###########################
-    defupdate(self):
-        self.dibujar_3t_on([])
+    def update(self):
+        self.dibujar_3t_on()
         self.pantalla.blit(self.pantalla_trans,(0,0))
 
 
@@ -237,12 +273,12 @@ class Tablero3:
         return mini_victorias_2T
 
     # Condición de victoria total
-    def victoria_3t(self, mini_victorias_2T: list):
+    def victoria_3t(self):
 
         # Transformamos mini_victorias en un array de 3x3
         victoria_array = np.array([[a * 3 + b for b in range(3)] for a in range(3)], dtype=np.dtype('U2'))
 
-        for (fila, columna) in mini_victorias_2T:
+        for (fila, columna) in self.mini_victorias_2T:
             victoria_array[fila, columna] = self.tablero[fila, columna, 0, 0]
 
         # Verificar filas
