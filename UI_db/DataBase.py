6169adef63b451f88e68a5ca0b621931d63e98ab
Guisplay <148806563+Guisplay@users.noreply.github.com>
2024-05-11 12:17:13 +0200

    Add files via upload

--- UI_db/DataBase.py
+++ UI_db/DataBase.py
@@ -1,189 +1,217 @@
 """ DataBase.py
 
-Este fichero contiene la creación de la Base de Datos. Además, en este script
-se desarrollan todos los ajustes y las comprobaciones necesarias.
+Este archivo es fundamental para la creación y gestión de la Base de Datos. En este script, se llevan a cabo todas las configuraciones
+necesarias y se realizan las comprobaciones pertinentes para asegurar el correcto funcionamiento de la base de datos.
 
-Para utilizar el código, no es necesario instalar nada ya que Python incluye la librería sqlite3.
+El uso de este código es sencillo y no requiere la instalación de ninguna librería adicional, 
+ya que Python incluye de forma nativa la librería sqlite3, que es la que se utiliza en este script.
 
-El fichero puede ser importado como módulo para utilizar todas las funciones.
+Este archivo puede ser importado como un módulo, lo que permite acceder y utilizar todas las funciones que contiene, 
+facilitando así su integración en otros ficheros.
 
 Bibliografía
 ------------
-    Codingraph (2022). Crear Base de Datos con Python.
-    Youtube. https://youtu.be/iH7d2vHVCUk?si=k0Dhb6LJQwRh7kVc
-
+    Codingraph (2022). "Crear Base de Datos con Python". Disponible en Youtube. 
+    Enlace: https://youtu.be/iH7d2vHVCUk?si=k0Dhb6LJQwRh7kVc
 """
+
+
 import sqlite3
 
-conexion = sqlite3.connect('Usuarios')  # nos conectamos a una DB
-cursor = conexion.cursor()              # creación del cursor
-
-
-def crearTabla(nombreTabla:str):
-    """ Comprueba si la tabla USUARIOS existe, si no existe crea la Tabla con todos sus atributos.
-
-    Parámetros
-    ----------
-    nombreTabla: str
-        Representa el nombre de la tabla que queremos comprobar
-
-    Devuelve
-    --------
-    bool
-        Un valor booleano que indica si la Tabla existe o no
+class DataBase:
     """
+    Clase DataBase que permite la creación y manipulación de una base de datos SQLite.
+    """
+    def __init__(self):
+        """
+        Constructor de la clase DataBase. Inicializa la conexión a la base de datos y crea un cursor.
+        """
+        self.conexion = sqlite3.connect('Usuarios')  # nos conectamos a una DB
+        self.cursor = self.conexion.cursor()              # creación del self.cursor
+
+    def __str__(self):
+        """
+        Método especial que devuelve una representación en cadena de la instancia de DataBase.
+        """
+        return f""" 
+Usuario: {self.returnActivo()[0]}
+Puntuación T1: {self.returnActivo()[2]}
+Puntuación T2: {self.returnActivo()[3]}
+Puntuación T3: {self.returnActivo()[4]}
+Puntuación M35: {self.returnActivo()[5]}
+"""
     
-    # Query
-    cursor.execute(f'''SELECT COUNT(name) FROM SQLITE_MASTER WHERE TYPE = 'table' AND name = '{nombreTabla}' ''')
+    def crearTabla(self, nombreTabla:str):
+        """ Comprueba si la tabla USUARIOS existe, si no existe crea la Tabla con todos sus atributos.
 
-    # La tabla existe
-    if cursor.fetchone()[0] >= 1:                           # fetchone para devolver una sola fila de la consulta
-        print(f'Usando la tabla {nombreTabla} existente')   # si devuelve uno o más resultados, encontró la tabla
-        return True
+        Parámetros
+        ----------
+        nombreTabla: str
+            Representa el nombre de la tabla que queremos comprobar
 
-    # La tabla no existe
-    else:
-        cursor.execute(f'''CREATE TABLE {nombreTabla} (
-                       NOMBRE TEXT PRIMARY KEY, 
-                       CONTRASEÑA TEXT, 
-                       FOTO TEXT, 
-                       ACTIVO BOOLEAN DEFAULT 0, 
-                       T1 INT DEFAULT 0,
-                       T2 INT DEFAULT 0,
-                       T3 INT DEFAULT 0,
-                       M35 INT DEFAULT 0)''')
+        Devuelve
+        --------
+        bool
+            Un valor booleano que indica si la Tabla existe o no
+        """
         
-        print(f'Tabla {nombreTabla} creada con éxito')
-        return False
+        # Query
+        self.cursor.execute(f'''SELECT COUNT(name) FROM SQLITE_MASTER WHERE TYPE = 'table' AND name = '{nombreTabla}' ''')
+
+        # La tabla existe
+        if self.cursor.fetchone()[0] >= 1:                           # fetchone para devolver una sola fila de la consulta
+            print(f'Usando la tabla {nombreTabla} existente')   # si devuelve uno o más resultados, encontró la tabla
+            return True
+
+        # La tabla no existe
+        else:
+            self.cursor.execute(f'''CREATE TABLE {nombreTabla} (
+                        NOMBRE TEXT PRIMARY KEY, 
+                        CONTRASEÑA TEXT, 
+                        FOTO TEXT, 
+                        ACTIVO BOOLEAN DEFAULT 0, 
+                        T1 INT DEFAULT 0,
+                        T2 INT DEFAULT 0,
+                        T3 INT DEFAULT 0,
+                        M35 INT DEFAULT 0)''')
+            
+            print(f'Tabla {nombreTabla} creada con éxito')
+            return False
     
-def setInactivo():
-    """ 
-    Establece todos los usuarios de la DB como inactivos
-    """
-    cursor.execute(''' UPDATE USUARIOS SET ACTIVO = 0''')
-    conexion.commit() # lo guardamos permanentemente
 
-def setActivo(nombre:str):
-    """ Establece un usuario en concreto al estado Activo
+    def setInactivo(self):
+        """ 
+        Establece todos los usuarios de la DB como inactivos
+        """
+        self.cursor.execute(''' UPDATE USUARIOS SET ACTIVO = 0''')
+        self.conexion.commit() # lo guardamos permanentemente
 
-    Parámetros
-    ----------
-    nombre: str
-        Representa la Primary Key del usuario que modificaremos
-    """
-    cursor.execute(f'''UPDATE USUARIOS SET ACTIVO=1 WHERE NOMBRE = '{nombre}' ''')
+    def setActivo(self, nombre:str):
+        """ Establece un usuario en concreto al estado Activo
 
-def returnActivo():
-    """
-    Busca el usuario activo en la sesión actual
+        Parámetros
+        ----------
+        nombre: str
+            Representa la Primary Key del usuario que modificaremos
+        """
+        self.cursor.execute('''UPDATE USUARIOS SET ACTIVO=1 WHERE NOMBRE = '{}' '''.format(nombre))
 
-    Devuelve
-    -------
-    tuple
-        Devuelve la tupla: (nombre, foto, T1, T2, T3)
-    """
-    cursor.execute(''' SELECT NOMBRE FROM USUARIOS WHERE ACTIVO = 1 ''')
-    activo = cursor.fetchall()
-    return activo[0] 
 
-def insertarUsuario(nombre:str, contraseña:str):
-    """
-    Inserta un usuario junto con una contraseña en la base de datos
-    """
-    try:
-        cursor.execute('''INSERT INTO USUARIOS (NOMBRE, CONTRASEÑA, FOTO) VALUES (?,?,'T1/Imagenes/UI/Menu/foto_default.jpeg') ''', (nombre, contraseña))
-        conexion.commit() # realizar los cambios
-    except sqlite3.IntegrityError:
-        print(f'\n\n--- Error en Insertar: PRIMARY KEY DUPLICADA: {nombre} ---')
-               
-def buscarUsuario(nombre: str, contraseña: str):
-    """
-    Busca un usuario específico en base a un usuario y contraseña
+    def returnActivo(self):
+        """
+        Busca el usuario activo en la sesión actual
 
-    Parámetros
-    ----------
-    nombre: str
-        Representa la PK del usuario
-    contraseña: str
-        Representa un atributo del usuario
+        Devuelve
+        -------
+        tuple
+            Devuelve la tupla: (nombre, foto, T1, T2, T3, M35)
+        """
+        self.cursor.execute(''' SELECT NOMBRE, foto, T1, T2, T3, M35 FROM USUARIOS WHERE ACTIVO = 1 ''')
+        activo = self.cursor.fetchall()
+        return activo[0] 
 
-    Devuelve: int
-        Devuelve 3 tipos de resultados [-1,0,1], dependiendo de la búsqueda realizada
-    """
+    def insertarUsuario(self, nombre:str, contraseña:str):
+        """
+        Inserta un usuario junto con una contraseña en la base de datos
+        """
+        try:
+            self.cursor.execute('''INSERT INTO USUARIOS (NOMBRE, CONTRASEÑA, FOTO) VALUES (?,?,'T1/Imagenes/UI/Menu/foto_default.jpeg') ''', (nombre, contraseña))
+            self.conexion.commit() # realizar los cambios
+        except sqlite3.IntegrityError:
+            print(f'\n\n--- Error en Insertar: PRIMARY KEY DUPLICADA: {nombre} ---')
+                
+    def buscarUsuario(self, nombre: str, contraseña: str):
+        """
+        Busca un usuario específico en base a un usuario y contraseña
 
-    cursor.execute('''SELECT * FROM USUARIOS WHERE NOMBRE = '{}' '''.format(nombre))
-    usuario_encontrado = cursor.fetchall() # obtenemos todos los resultados de la query
+        Parámetros
+        ----------
+        nombre: str
+            Representa la PK del usuario
+        contraseña: str
+            Representa un atributo del usuario
 
-    # No se encuentra ningún usuario
-    if usuario_encontrado == []:
-        print(f'El usuario {nombre} no está registrado')
-        return -1
-    
-    # Se encuentra un posible usuario
-    else:
-        cursor.execute("SELECT * FROM USUARIOS WHERE NOMBRE = ? AND CONTRASEÑA = ?" , (nombre, contraseña))
-        usuario_encontrado = cursor.fetchall()
-        # La contraseña no es correcta
+        Devuelve: int
+            Devuelve 3 tipos de resultados [-1,0,1], dependiendo de la búsqueda realizada
+        """
+
+        self.cursor.execute('''SELECT * FROM USUARIOS WHERE NOMBRE = '{}' '''.format(nombre))
+        usuario_encontrado = self.cursor.fetchall() # obtenemos todos los resultados de la query
+
+        # No se encuentra ningún usuario
         if usuario_encontrado == []:
-            print('Contraseña incorrecta')
-            return 0
-        # la contraseña es correcta
-        else:
-            print('Inicio de sesión exitoso')
-            return 1
-
-def añadirPuntuacion(PK:str, juego:str, cantidad:int):
-    """
-    Aumenta la puntuación de un usuario
-
-    Parámetros
-    ----------
-    PK: str
-        Representa la Primary Key del usuario que queremos modificar
-    juego: str
-        Representa el juego donde se produce el cambio de puntuación
-    cantidad: int
-        Representa la cantidad de puntuación que añadiremos
-    """
-    cursor.execute(''' UPDATE USUARIOS SET {} = {}+{} WHERE NOMBRE = '{}' '''.format(juego,juego,cantidad,PK))
-    conexion.commit() # ejecutar los cambios
-
-def mostrarDatos():
-    """
-    Muestra todos los datos de todos los usuarios en la DB
-    """
-    cursor.execute('''
-        SELECT * FROM USUARIOS
-        ''')
-    lista = []
-    for filaEncontrada in cursor.fetchall():
-        lista.append(filaEncontrada) 
-    print(lista)
-
-
-# MÉTODOS SIN UTILIZAR
-def modificarAtributo(nombre, diccionario: dict):
-    valoresValidos = ['NOMBRE','CONTRASEÑA', 'FOTO', 'ACTIVO']
-    for key in diccionario.keys():
-        if key not in valoresValidos:
-            print('Esa columna no existe')
+            print(f'El usuario "{nombre}" no está registrado')
+            return -1
         
+        # Se encuentra un posible usuario
         else:
-            query = '''UPDATE USUARIOS SET {} = '{}' WHERE NOMBRE = '{}' '''.format(key,diccionario[key], nombre)
-            try:
-                cursor.execute(query)
-                conexion.commit()
-            except sqlite3.IntegrityError:
-                print('\n\n--- Error_update: PRIMARY KEY DUPLICADA:', diccionario['NOMBRE'], ' ---\n')
-            except sqlite3.OperationalError:
-                print(f'\n\n--- Error_update: NO SE ENCONTRÓ LA PRIMARY KEY {nombre} ---\n')
+            self.cursor.execute("SELECT * FROM USUARIOS WHERE NOMBRE = ? AND CONTRASEÑA = ?" , (nombre, contraseña))
+            usuario_encontrado = self.cursor.fetchall()
+            # La contraseña no es correcta
+            if usuario_encontrado == []:
+                print('Contraseña incorrecta')
+                return 0
+            # la contraseña es correcta
+            else:
+                print('Inicio de sesión exitoso')
+                return 1
 
-def eliminarUsuario(nombre):
-    try:
-        cursor.execute('''
-        DELETE FROM USUARIOS WHERE NOMBRE = '{}'
-        '''.format(nombre))
-        conexion.commit()
+    def añadirPuntuacion(self, PK:str, juego:str, cantidad:int):
+        """
+        Aumenta la puntuación de un usuario
 
-    except sqlite3.OperationalError:
-        print(f'\n\n--- Error_delete: NO SE HA ENCONTRADO: {nombre} ---\n')
+        Parámetros
+        ----------
+        PK: str
+            Representa la Primary Key del usuario que queremos modificar
+        juego: str
+            Representa el juego donde se produce el cambio de puntuación
+        cantidad: int
+            Representa la cantidad de puntuación que añadiremos
+        """
+        self.cursor.execute(''' UPDATE USUARIOS SET {} = {}+{} WHERE NOMBRE = '{}' '''.format(juego,juego,cantidad,PK))
+        self.conexion.commit() # ejecutar los cambios
+
+    def mostrarDatos(self):
+        """
+        Muestra todos los datos de todos los usuarios en la DB
+        """
+        self.cursor.execute('''
+            SELECT * FROM USUARIOS
+            ''')
+        lista = []
+        for filaEncontrada in self.cursor.fetchall():
+            lista.append(filaEncontrada) 
+        print(lista)
+
+
+    # MÉTODOS SIN UTILIZAR
+    def modificarAtributo(self, nombre, diccionario: dict):
+        valoresValidos = ['NOMBRE','CONTRASEÑA', 'FOTO', 'ACTIVO']
+        for key in diccionario.keys():
+            if key not in valoresValidos:
+                print('Esa columna no existe')
+            
+            else:
+                query = '''UPDATE USUARIOS SET {} = '{}' WHERE NOMBRE = '{}' '''.format(key,diccionario[key], nombre)
+                try:
+                    self.cursor.execute(query)
+                    self.conexion.commit()
+                except sqlite3.IntegrityError:
+                    print('\n\n--- Error_update: PRIMARY KEY DUPLICADA:', diccionario['NOMBRE'], ' ---\n')
+                except sqlite3.OperationalError:
+                    print(f'\n\n--- Error_update: NO SE ENCONTRÓ LA PRIMARY KEY {nombre} ---\n')
+
+    def eliminarUsuario(self, nombre):
+        try:
+            self.cursor.execute('''
+            DELETE FROM USUARIOS WHERE NOMBRE = '{}'
+            '''.format(nombre))
+            self.conexion.commit()
+
+        except sqlite3.OperationalError:
+            print(f'\n\n--- Error_delete: NO SE HA ENCONTRADO: {nombre} ---\n')
+
+
+# Bases de Datos Instanciadas
+db_principal = DataBase()
