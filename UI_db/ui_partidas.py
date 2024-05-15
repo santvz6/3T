3ae09254fa84e0fb4f18e6d700e2b0245efe4c79
Santiago <148806563+santvz6@users.noreply.github.com>
2024-05-15 20:51:22 +0200

    Add files via upload

--- UI_db/ui_partidas.py
+++ UI_db/ui_partidas.py
@@ -1,7 +1,9 @@
 from customtkinter import *
 from PIL import Image
+import os
 import pandas as pd
 import pygame as pg
+
 
 class Partidas3T(CTkToplevel):
     def __init__(self, master):
@@ -9,11 +11,14 @@ class Partidas3T(CTkToplevel):
                                         # ventana 'principal'(UiMenu) a la clase 
                                         # padre CTkTopLevel
         
-        #print(CTkToplevel.__bases__) # Para ver ºe donde hereda CTkTopLevel
-
-        self.title('P')
+        self.title('Cagar - Guardar Partidas')
         self.geometry('1280x720+200+40')
         self.resizable(0,0)
+
+        # https://stackoverflow.com/questions/75825190/how-to-put-iconbitmap-on-a-customtkinter-toplevel
+        # En un foro de stackoverflow se menciona que trabajar con iconbitmap cuando se hereda de TopLevel
+        # ocasiona problemas debido a que customtkinter cambia la foto del icono a las 250 milésimas de heredar.
+        self.after(250, lambda: self.iconbitmap(('./Imagenes/UI/TTT.ico')))
 
         self.matriz_cargada = None
 
@@ -27,22 +32,22 @@ class Partidas3T(CTkToplevel):
         # Partida Seleccionada
         partida = CTkImage(Image.open('./Imagenes/Partidas/Default.png'), size=(500,281)) # la abrimos con PIL dentro de un CTkImage 
         self.partida = CTkLabel(self, image=partida, text='', bg_color='#fceee2')  # mostramos la foto en una etiqueta
-        self.partida.place(relx=0.705, rely=0.4, anchor='center') 
+        self.partida.place(relx=0.705, rely=0.40264, anchor='center') 
 
     # BOTONES
         # Cargar
         cargar = CTkButton(self.fondo, 
-                         hover_color='#caa481', fg_color='#a2857a', bg_color='#f3e6db',
-                         corner_radius=5,
-                         text='Cargar', text_color='#dec1a5', font=('TypoGraphica',14),
-                         width=250, height=55, command=self.cargarID)
+            hover_color='#957569', fg_color='#8a6a5e', bg_color='#f3e6db',
+            border_width=2.5, border_color='#ccb3a8', corner_radius=0, 
+            text='Cargar', text_color='#ffffff', font=('TypoGraphica',14),
+            width=250, height=55, command=self.cargarID)
         cargar.place(relx=0.605, rely=0.78, anchor = 'center')
         # Borrar
         borrar = CTkButton(self.fondo, 
-                         hover_color='#caa481', fg_color='#a2857a', bg_color='#f3e6db',
-                         corner_radius=5,
-                         text='Borrar', text_color='#dec1a5', font=('TypoGraphica',14),
-                         width=250, height=55)
+            hover_color='#957569', fg_color='#8a6a5e', bg_color='#f3e6db',
+            border_width=2.5, border_color='#ccb3a8', corner_radius=0, 
+            text='Borrar', text_color='#ffffff', font=('TypoGraphica',14),
+            width=250, height=55)
         borrar.place(relx=0.805, rely=0.78, anchor = 'center')
 
 #############################   LADO IZQUIERDO  ############################# 
@@ -50,24 +55,26 @@ class Partidas3T(CTkToplevel):
     # BOTONES
         # Buscar
         buscar = CTkButton(self, 
-            hover_color='#123456', fg_color='#654321', bg_color='#ffffff',
-            corner_radius=5,
+            hover_color='#957569', fg_color='#8a6a5e', bg_color='#f3e6db',
+            border_width=2.5, border_color='#ccb3a8', corner_radius=0, 
             text='Buscar', text_color='#ffffff', font=('TypoGraphica',14),
-            width=180, height=50)
-        buscar.place(relx=0.156, rely=0.694, anchor = 'center')
+            width=178, height=50)
+        buscar.place(relx=0.157, rely=0.6935, anchor = 'center')
         # Guardar
         guardar = CTkButton(self, 
-            hover_color='#123456', fg_color='#654321', bg_color='#ffffff',
-            corner_radius=5,
+            hover_color='#957569', fg_color='#8a6a5e', bg_color='#f3e6db',
+            border_width=2.5, border_color='#ccb3a8', corner_radius=0, 
             text='Guardar', text_color='#ffffff', font=('TypoGraphica',14),
-            width=180, height=50, command=self.guardarID)
+            width=178, height=50, command=self.guardarID)
         guardar.place(relx=0.33, rely=0.694, anchor = 'center')
+    
+    # TEXTBOX    
         # Input
         self.input = CTkTextbox(self,width=400,height=50,
-            border_width=0,    
-            font=('TypoGraphica',20),   
-            fg_color='#123456', bg_color='#123456',
-            text_color='#000000')
+            fg_color='#a2857a', bg_color='#f3e6db',
+            border_width=2.5, border_color='#ccb3a8', corner_radius=0, 
+            font=('Bahnschrift Bold Semi-Condensed',20),   
+            text_color='#ffffff')
         self.input.place(relx=0.245, rely=0.563, anchor = 'center')
 
 
@@ -87,53 +94,71 @@ class Partidas3T(CTkToplevel):
         self.withdraw()         # ocultamos Descripción
 
     def cargarID(self):
-        ID = self.cargar_inp.get('0.0', 'end')[:-1]
-
-        # Leer el archivo CSV en un DataFrame de pandasr
-        df = pd.read_csv('./Partidas/Partidas3T.csv', dtype=str)
+        ID = self.input.get('0.0', 'end')[:-1]
 
         try:
-            # Filtrar el DataFrame para obtener la matriz deseada
-            df_matriz = df[ID]
-        except KeyError as e:
-            print(f'No se encontró {ID}. Excepción: {e}')
-            self.matriz_cargada = None
-        else:    
-            # Convertir el DataFrame filtrado a una matriz de NumPy
-            tablero_aplanado = df_matriz.values
+            # Leer el archivo CSV en un DataFrame de pandasr
+            df = pd.read_csv('./Partidas/Partidas3T.csv', dtype=str)
+        except FileNotFoundError as e:
+            print(f'{e}: Archivo CSV borrado')
+        else:
+            try:
+                # Filtrar el DataFrame para obtener la matriz deseada
+                matriz_cargada = df[ID]
+            except KeyError as e:
+                print(f'{e}: No se encontró {ID}')
+                self.matriz_cargada = None
+                self.cambiarFoto(None)
+            else:    
+                # Convertir el DataFrame filtrado a una matriz de NumPy
+                tablero_aplanado = matriz_cargada.values
+                # Reconstruir la matriz de 6 dimensiones
+                forma_original = (3, 3, 3, 3, 3, 3)  # Esta debería ser la forma original de tu matriz
+                self.matriz_cargada = tablero_aplanado.reshape(forma_original)
 
-            # Reconstruir la matriz de 6 dimensiones
-            forma_original = (3, 3, 3, 3, 3, 3)  # Esta debería ser la forma original de tu matriz
-            
-            self.matriz_cargada = tablero_aplanado.reshape(forma_original)
+                self.cambiarFoto(ID)
 
 
     def guardarID(self):
-        ID = self.cargar_inp.get('0.0', 'end')[:-1]
-        tablero = self.master.master.main.pantalla_actual.t3_set.tablero
 
-        # Tu diccionario
+        ID = self.input.get('0.0', 'end')[:-1]
+        tablero = self.master.master.main.pantalla_actual.t3_set.tablero
         data = {ID: tablero.flatten()}
         
+        # Crear - Modificar .csv
         try:
             csv = open('./Partidas/Partidas3T.csv')
 
         except FileNotFoundError as e:
             df = pd.DataFrame(data)
-            df.to_csv('./Partidas/Partidas3T.csv', index=True)
-            print(f'Creando DataFrame: {e}')
+            df.to_csv('./Partidas/Partidas3T.csv', index=True) # index True es necesario?
+            print(f'{e}: Creando DataFrame')
 
         else:
             csv.close()
-            # Si existe, lee el archivo
+            print('Modificando CSV existente.')
             df = pd.read_csv('./Partidas/Partidas3T.csv')
             df[ID] = tablero.flatten()
             
-            pg.image.save(pg.display.get_surface(), f"./Partidas/{ID}.png")
-            self.fondo.configure(image = f'./Partidas/{ID}.png')
+            # Guardar Imagen
+            try:
+                os.rename('./Partidas/EnEspera.png', f'./Partidas/{ID}.png')
+            except FileNotFoundError as e:
+                print(f'{e}: Foto de Partida "EnEspera" eliminada')
+            else:
+                foto_guardada = CTkImage(Image.open(f'./Partidas/{ID}.png'), size=(500,281))
+                self.partida.configure(image = foto_guardada)
+        finally:
+            # Guarda el DataFrame en un archivo .csv
+            df.to_csv('./Partidas/Partidas3T.csv', index=False)
 
-        # Guarda el DataFrame en un archivo .csv
-        df.to_csv('./Partidas/Partidas3T.csv', index=False)
+    def cambiarFoto(self, ID):
+        try:
+            foto_cargada = CTkImage(Image.open(f'./Partidas/{ID}.png'), size=(500,281))
+        except FileNotFoundError as e:
+            print(f'{e}: Foto Partida Cargada')
+        else:
+            self.partida.configure(image=foto_cargada)
     
 
 
