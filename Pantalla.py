476936f80dbb31808eef531d688ea96ec952b460
Santiago <148806563+santvz6@users.noreply.github.com>
2024-05-22 19:35:53 +0200

    Update Pantalla.py

--- Pantalla.py
+++ Pantalla.py
@@ -215,7 +215,7 @@ class Pantalla:
                     a = self.m35_set.actualizar_m35_mouse()
                     if not self.m35_set.victoria_m35()[0] and self.m35_set.num_mov < 25:    
                         if a != False:                 
-                            if self.m35_set.num_mov == 0:
+                            if a not in self.m35_set.restriccion:
                                 self.m35_set.validar(self.m35_set.actualizar_m35_mouse())
 
                             elif a in self.m35_set.restriccion:
