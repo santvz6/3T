3653d0c0c27769030f493522b60bf38b16cd3670
paumateo22 <152263653+paumateo22@users.noreply.github.com>
2024-05-22 23:42:53 +0200

    Update Pantalla.py

--- Pantalla.py
+++ Pantalla.py
@@ -440,7 +440,7 @@ class Pantalla:
 # *BUCLE* #######################        M35        ###################################
         elif self.cambio_pantalla == 'm35':
 
-            if not self.m35_set.victoria_m35()[0]  and self.m35_set.return_num_mov() < 25:   # NO hay victoria
+            if not self.m35_set.victoria_m35()[0]  and self.m35_set.return_num_mov < 25:   # NO hay victoria
                 self.m35_set.update()    # update está creado en M35_settings → t1_set (update es la forma correcta para ejcutar M35)
 
             else:                                                                       # SÍ hay victoria
